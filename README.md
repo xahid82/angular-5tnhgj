3be39f58bf8f7a3068a994c56f20cb226d60ae27
xahid82 <zahid.siddique@gmail.com>
2020-04-12 00:57:42 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-5tnhgj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-5tnhgj)
---
+++ app.module.ts
@@ -0,0 +1,37 @@
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgOptionHighlightModule } from '@ng-select/ng-option-highlight';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { DataSourceOptionsExampleComponent } from './src/data-source-options-example.component'
+
+@Component({
+    selector: 'app-component',
+    template: '<data-source-options-example></data-source-options-example>'
+})
+export class AppComponent {}
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgSelectModule,
+        NgOptionHighlightModule,
+        CommonModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    declarations: [
+        AppComponent,
+        DataSourceOptionsExampleComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+
+
